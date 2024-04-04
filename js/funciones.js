9a9d45715ad983758a708d87adbf119d6c962d1f
Luna Marrano <lunamarrano2@gmail.com>
2024-04-04 20:55:19 -0300

    Agregue boton "suma"

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,10 @@
-function Saludar(){
-    alert("hola mundo");
+function Saludar(nombre){
+    var nombre = "Luna";
+    alert("Hola "+nombre);
+}
+function Suma(resultado){
+    var num1=5;
+    var num2=5;
+    result=num1+num2;
+    alert("El resultado es: "+result);
 }
