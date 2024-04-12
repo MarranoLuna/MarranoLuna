8bee5ee56a468298cfebb3eb5e7da36b40aaafff
Luna Marrano <lunamarrano2@gmail.com>
2024-04-11 21:31:25 -0300

    Cree boton buscar , el cual busca el nombre del personaje

--- TP1/fn.js
+++ TP1/fn.js
@@ -1,4 +1,4 @@
-const frutas=['pera','manzana','frutilla','kiwi','naranja'];
+/*const frutas=['pera','manzana','frutilla','kiwi','naranja'];
 
 // 3 formas de recorrer un array
 
@@ -16,4 +16,17 @@ console.log(' ');
 
 for(const fruta of frutas){
     if (fruta=='manzana') console.log(fruta);
+}*/
+
+
+function buscar(){
+    var valor=document.getElementById('numero').value;
+    fetch('https://rickandmortyapi.com/api/character/' + valor)
+    .then (response=>response.json())
+    .then (data=>{
+            console.log(data.name)
+    });
+
 }
+
+//document.getElementById("numero").innerHTML="El personaje "+data.name+" esta "+data.status
--- TP1/json.js
+++ TP1/json.js
@@ -1,33 +1,11 @@
 
-const personaje={
-    "id": 140,
-    "name": "Genital Washer",
-    "status": "Alive",
-    "species": "Human",
-    "type": "",
-    "gender": "Male",
-    "origin": {
-        "name": "Post-Apocalyptic Earth",
-        "url": "https://rickandmortyapi.com/api/location/8"
-    },
-    "location": {
-        "name": "Post-Apocalyptic Earth",
-        "url": "https://rickandmortyapi.com/api/location/8"
-    },
-    "image": "https://rickandmortyapi.com/api/character/avatar/140.jpeg",
-    "episode": [
-        "https://rickandmortyapi.com/api/episode/23"
-    ],
-    "url": "https://rickandmortyapi.com/api/character/140",
-    "created": "2017-12-27T18:47:44.566Z"
-}
+
+    
+
 
 //console.log(personaje);
 //console.log(personaje.name);
 //console.log(personaje.origin.url);  
-
-document.getElementById("result").innerHTML="El personaje "+personaje.name+" esta "+personaje.status;
-
 
 
 /*
